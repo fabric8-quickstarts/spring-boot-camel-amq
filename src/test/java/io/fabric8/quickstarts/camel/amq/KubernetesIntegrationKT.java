28b72804709303f0283ed92366e6832aa0d883ba
Nicola Ferraro <ni.ferraro@gmail.com>
2016-11-11 14:58:57 +0100

    OSFUSE-390: remove warning

--- src/test/java/io/fabric8/quickstarts/camel/amq/KubernetesIntegrationKT.java
+++ src/test/java/io/fabric8/quickstarts/camel/amq/KubernetesIntegrationKT.java
@@ -16,6 +16,8 @@
 package io.fabric8.quickstarts.camel.amq;
 
 import io.fabric8.kubernetes.client.KubernetesClient;
+
+import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.junit.Test;
@@ -24,6 +26,7 @@ import org.junit.runner.RunWith;
 import static io.fabric8.kubernetes.assertions.Assertions.assertThat;
 
 @RunWith(Arquillian.class)
+@RunAsClient
 public class KubernetesIntegrationKT {
 
     @ArquillianResource
